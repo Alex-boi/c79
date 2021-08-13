a7939c8c95e5e04348de4beae0936ec9e41212c1
Ximenal <83797900+Alex-boi@users.noreply.github.com>
2021-08-12 19:32:45 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 child=[];
 
 function submit(){
-var child1=document.getElementById("c1").Value;
-var child2=document.getElementById("c2").Value;
-var child3=document.getElementById("c3").Value;
-var child4=document.getElementById("c4").Value;
+var child1=document.getElementById("c1").value;
+var child2=document.getElementById("c2").value;
+var child3=document.getElementById("c3").value;
+var child4=document.getElementById("c4").value;
 child.push(child1);
 child.push(child2);
 child.push(child3);
@@ -17,6 +17,6 @@ document.getElementById("buttonsort").style.display="inline-block";
 
 function sorting(){
     child.sort();
-    console.log(child);
+    console.log(child); 
     document.getElementById("children").innerHTML=child;
 }
